279da1926afe7fad8f15d0baec410775a4b62bbc
Suraj8108 <suraj98693@gmail.com>
2023-01-21 18:39:41 +0530

    Site Link Changed

--- src/Service/AuthService.js
+++ src/Service/AuthService.js
@@ -3,8 +3,8 @@ import { toast } from "react-toastify";
 import setTimeout  from "react";
 
 //Get The Token
-// const sitelink="http://ec2-3-110-171-177.ap-south-1.compute.amazonaws.com:9900"
-const sitelink="http://localhost:9969"
+const sitelink="http://ec2-3-110-171-177.ap-south-1.compute.amazonaws.com:9900"
+// const sitelink="http://localhost:9969"
 
 const authService = async (JwtData) => {
     await fetch(`${sitelink}/token`, {
